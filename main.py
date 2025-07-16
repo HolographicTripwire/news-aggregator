06a010997bb27befd5542b5ecf2d63d5bfa30e6d
HolographicTripwire <josephsmagen@gmail.com>
2025-07-16 16:04:53 +0100

    Rewrite main.py to account for multiple sources

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
 import sources.newsapi as newsapi
+import sources.rss as rss
+from sources import no_repeats, round_robin
 
 def main():
-    for article in newsapi.top_articles(country='us',category='technology'):
-        print(article.title)
+    newsapi_sources = [
+        newsapi.top_articles(country='us'),
+        newsapi.top_articles(country='gb'),
+    ]
+    rss_sources = [rss.articles_from_rss_feed(url) for url in rss.news_rss_urls]
+    sources = newsapi_sources + rss_sources
+    for article in no_repeats(round_robin(sources)):
+        print(article.to_string() + "\n")
 
 if __name__ == "__main__":
     main()
